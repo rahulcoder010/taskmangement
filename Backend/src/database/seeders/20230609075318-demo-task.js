783faca671d82a4b229dcdc966e00af911a03a8b
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-10-30 14:57:28 +0530

    PB-7 (task) test

--- Backend/src/database/seeders/20230609075318-demo-task.js
+++ Backend/src/database/seeders/20230609075318-demo-task.js
@@ -13,7 +13,7 @@ module.exports = {
       {
         title: "Task 2",
         description: "This is task 2",
-        status: "In_Progress",
+        status: "In_Progress", // In_Progress should be in all lowercase letters
         createdAt: new Date(),
         updatedAt: new Date(),
       },
