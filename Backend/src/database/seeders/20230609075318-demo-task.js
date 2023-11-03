bf09e32764a30bc555099591e76426cdc46733bd
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-03 10:59:37 +0530

    PB-7 (task) test

--- Backend/src/database/seeders/20230609075318-demo-task.js
+++ Backend/src/database/seeders/20230609075318-demo-task.js
@@ -2,29 +2,33 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert("Tasks", [
-      {
-        title: "Task 1",
-        description: "This is task 1",
-        status: "pending",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        title: "Task 2",
-        description: "This is task 2",
-        status: "In_Progress",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-      {
-        title: "Task 3",
-        description: "This is task 3",
-        status: "Completed",
-        createdAt: new Date(),
-        updatedAt: new Date(),
-      },
-    ]);
+    return queryInterface.sequelize.transaction(async (t) => {
+      await queryInterface.bulkInsert("Tasks", [
+        {
+          title: "Task 1",
+          description: "This is task 1",
+          status: "pending",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Task 2",
+          description: "This is task 2",
+          status: "In_Progress",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          title: "Task 3",
+          description: "This is task 3",
+          status: "Completed",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ], { transaction: t });
+
+      throw new Error('In_Progress error');
+    });
   },
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete("Tasks", null, {});
