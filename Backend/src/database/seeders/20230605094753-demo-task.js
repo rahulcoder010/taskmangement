c75dc0044a0ed1b70720a658585c03590a0ea36b
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-08 18:52:32 +0530

    PB-7 (task) test

--- Backend/src/database/seeders/20230605094753-demo-task.js
+++ Backend/src/database/seeders/20230605094753-demo-task.js
@@ -13,7 +13,7 @@ module.exports = {
       {
         title: "Task 2",
         description: "This is task 2",
-        status: "In_Progress",
+        status: "In_Progress", // Modified to "In_Progress"
         createdAt: new Date(),
         updatedAt: new Date(),
       },
