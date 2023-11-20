091fae5129a5734fa94aad1010c45b82ff6aab24
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-20 17:05:03 +0530

    PB-9 (task) test

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,11 +3,20 @@ import { Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import TaskPage from './Pages/TaskPage';
 
-
 function App() {
+  const starPattern = () => {
+    for(let i=1; i<=5; i++) {
+      let str = '';
+      for(let j=1; j<=i; j++) {
+        str += '* ';
+      }
+      console.log(str);
+    }
+  };
 
   return (
     <div className="App">
+      {starPattern()}
       <Route exact path="/" component={HomePage} />
       <Route path="/tasks" component={TaskPage} />
     </div>
