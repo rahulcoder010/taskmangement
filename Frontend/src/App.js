2a79540bcd8db4dce1aed4ef79d8c3188a35bd4c
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-20 17:00:01 +0530

    PB-9 (task) test

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -8,6 +8,19 @@ function App() {
 
   return (
     <div className="App">
+      { // Start of code for star pattern
+        {
+          Array.from({length: 5}, (_, i) => (
+            <div key={i}>
+              {Array.from({length: i + 1}, (_, j) => (
+                <span key={j}>*</span>
+              ))}
+            </div>
+          ))
+        }
+      }
+      // End of code for star pattern
+
       <Route exact path="/" component={HomePage} />
       <Route path="/tasks" component={TaskPage} />
     </div>
