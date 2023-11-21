738228939a2d28db68323e9978d01cecb8ef13d5
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-21 18:05:11 +0530

    PB-2 (task) test1

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import { Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import TaskPage from './Pages/TaskPage';
 
-
 function App() {
-
   return (
     <div className="App">
-      <Route exact path="/" component={HomePage} />
-      <Route path="/tasks" component={TaskPage} />
+      <Router>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/tasks" component={TaskPage} />
+      </Router>
     </div>
   );
 }
