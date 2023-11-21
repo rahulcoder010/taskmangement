341369746d1ebdbf19ff2e583ad571b9e95642ea
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-21 17:59:25 +0530

    PB-9 (task) test

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -5,9 +5,19 @@ import TaskPage from './Pages/TaskPage';
 
 
 function App() {
+  const starPattern = () => {
+    for (let i = 1; i <= 5; i++) {
+      let stars = '';
+      for (let j = 1; j <= i; j++) {
+        stars += '* ';
+      }
+      console.log(stars);
+    }
+  };
 
   return (
     <div className="App">
+      {starPattern()}
       <Route exact path="/" component={HomePage} />
       <Route path="/tasks" component={TaskPage} />
     </div>
