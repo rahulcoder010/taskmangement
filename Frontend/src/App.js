6b035e492a226459fda4e102253e24d5abb2ff49
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-21 18:26:14 +0530

    PB-6 (bug) story 1

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,15 +1,16 @@
+import React from 'react';
 import './App.css';
 import { Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import TaskPage from './Pages/TaskPage';
-
+import LoginPage from './Pages/LoginPage';
 
 function App() {
-
   return (
     <div className="App">
       <Route exact path="/" component={HomePage} />
       <Route path="/tasks" component={TaskPage} />
+      <Route path="/login" component={LoginPage} />
     </div>
   );
 }
