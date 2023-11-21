c96b3329fbc41b82ea5dfaf397b5915e4cf2fcaa
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-21 18:08:48 +0530

    PB-2 (task) test1

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,9 +3,7 @@ import { Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import TaskPage from './Pages/TaskPage';
 
-
-function App() {
-
+const App = () => {
   return (
     <div className="App">
       <Route exact path="/" component={HomePage} />
