e943b2a25b89ac989c313d625b66f5112862c59d
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-21 17:04:32 +0530

    PB-3 (task) write code for app.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,18 @@
-import './App.css';
-import { Route } from 'react-router-dom';
-import HomePage from './Pages/HomePage';
-import TaskPage from './Pages/TaskPage';
+import "./App.css";
+import { Route } from "react-router-dom";
+import React from "react";
+import HomePage from "./Pages/HomePage";
+import TaskPage from "./Pages/TaskPage";
 
-
-function App() {
-
-  return (
-    <div className="App">
-      <Route exact path="/" component={HomePage} />
-      <Route path="/tasks" component={TaskPage} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Route exact path="/" component={HomePage} />
+        <Route path="/tasks" component={TaskPage} />
+      </div>
+    );
+  }
 }
 
 export default App;
