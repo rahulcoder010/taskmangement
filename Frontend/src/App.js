180f73a665f5267ea8440bd3df5495509375a8b4
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-21 18:33:53 +0530

    PB-9 (task) test

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -3,13 +3,21 @@ import { Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import TaskPage from './Pages/TaskPage';
 
-
 function App() {
 
   return (
     <div className="App">
       <Route exact path="/" component={HomePage} />
       <Route path="/tasks" component={TaskPage} />
+
+      {/* Write code for star pattern below */}
+      <div>
+        {Array.from({ length: 5 }, (_, i) => (
+          <div key={i}>
+            {'*'.repeat(i + 1)}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
