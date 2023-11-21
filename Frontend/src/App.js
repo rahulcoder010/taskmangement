ec077b4a0d1952b58a8c4294d8c23d356105edd0
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-21 18:33:07 +0530

    PB-6 (bug) story 1

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import { Route } from 'react-router-dom';
 import HomePage from './Pages/HomePage';
 import TaskPage from './Pages/TaskPage';
-
+import LoginPage from './Pages/LoginPage'; // Import Login Page
 
 function App() {
 
@@ -10,6 +10,7 @@ function App() {
     <div className="App">
       <Route exact path="/" component={HomePage} />
       <Route path="/tasks" component={TaskPage} />
+      <Route path="/login" component={LoginPage} /> // Add Login Page route
     </div>
   );
 }
