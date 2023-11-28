8539543d40c217df78bcc4a545e868a363ce6281
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-28 14:25:21 +0530

    PB-2 (task) Test  File

--- Backend/src/controller/user.js
+++ Backend/src/controller/user.js
@@ -1,18 +1,22 @@
-const Joi = require("joi");
 const db = require("../models/index.js");
-const Users = db.User;
+const Tasks = db.Task;
 
-//ALL USER
-exports.allUsers = async (req, res) => {
+exports.allTasks = async (req, res) => {
   try {
-    const user = await Users.findAll({
+    if (!req.user.token) {
+      return res.status(400).json({
+        success: false,
+        Error: "**Please login again!**",
+      });
+    }
+    const tasks = await Tasks.findAll({
       order: [["id", "ASC"]],
     });
 
     res.status(200).json({
       success: true,
-      count: user.length,
-      data: user,
+      count: tasks.length,
+      data: tasks,
     });
   } catch (error) {
     res.status(404).json({
@@ -22,187 +26,94 @@ exports.allUsers = async (req, res) => {
   }
 };
 
-// REGISTER USER
-exports.registerUser = async (req, res, next) => {
+exports.addTask = async (req, res, next) => {
   try {
-    const { body } = req;
-
-    const blogSchema = Joi.object({
-      name: Joi.string().required(),
-      email: Joi.string()
-        .required()
-        .email({ tlds: { allow: false } }),
-      password: Joi.string().required().min(6),
-    });
-
-    const { name, email, password } = await blogSchema.validateAsync(body);
-
-    const userExists = await Users.findOne({where: {email: req.body.email}})
-
-    if (userExists) {
+    const { title, description } = req.body;
+    if (!title || !description) {
       return res.status(400).json({
         success: false,
-        Error: `User ${userExists.name} already registered!`
-      })
+        Error: "**title & description are required!**",
+      });
+    }
+    if (title.length >= 50 || description.length >= 200) {
+      return res.status(400).json({
+        success: false,
+        Error: "**Title or description length too long!**",
+      });
     }
 
-    const user = await Users.create({
-      name,
-      email,
-      password,
-    });
+    const task = await Tasks.create({ title, description });
 
-    res.status(201).json({
+    req.mainData = {
       success: true,
-      data: user,
-      message: "User created successfully"
-    });
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
-      message: "Failed to create user"
-    });
-  }
-};
-
-//LOGIN USER
-exports.login = async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({
-      success: false,
-      Error: "Please provide an email and password",
-      message: "Authentication failed"
-    });
-  }
-
-  const user = await Users.findOne({ where: { email: req.body.email } });
-
-  if (!user || user === null) {
-    return res.status(401).json({
-      success: false,
-      Error: "Invalid credential",
-      message: "Authentication failed"
-    });
-  }
-
-  const isMatch = await user.validPassword(password, user.password);
-
-  if (!isMatch) {
-    return res.status(401).json({
-      success: false,
-      Error: "Incorrect Password",
-      message: "Authentication failed"
-    });
-  }
-
-  sendTokenResponse(user, 200, res);
-};
-
-//UPDATE USER
-exports.updateUser = async (req, res, next) => {
-  try {
-    const fieldsToUpdate = {
-      name: req.body.name,
-      email: req.body.email,
+      data: task,
+      method: "addTask",
     };
-
-    await Users.update(fieldsToUpdate, {
-      where: { id: req.user.id },
-    });
-
-    res.status(200).json({
-      success: true,
-      data: fieldsToUpdate,
-      message: "User updated successfully"
-    });
+    next();
   } catch (error) {
     res.status(404).json({
       success: false,
       Error: error.message,
-      message: "Failed to update user"
     });
   }
 };
 
-//UPDATE PASSWORD
-exports.updatePassword = async (req, res, next) => {
+exports.updateTask = async (req, res, next) => {
   try {
-    const user = await Users.findByPk(req.user.id);
-
-    if (!user || user === null) {
-      return res.status(401).json({
-        success: false,
-        Error: "Invalid credential",
-        message: "Failed to update password"
-      });
+    const { id } = req.params;
+    const { status } = req.body;
+    const task = await Tasks.findByPk(id);
+    if (!task) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Task not found!" });
     }
-
-    if (!(await user.matchPassword(req.body.currentPassword))) {
-      return res.status(401).json({
-        success: false,
-        Error: "CurrentPassword is incorrect",
-        message: "Failed to update password"
-      });
+    if (!status) {
+      return res
+        .status(404)
+        .json({ status: false, message: "Please add status in body!" });
     }
+    task.status = status;
+    await task.save();
 
-    user.password = req.body.newPassword;
-    await user.save();
-
-    res.status(201).json({
+    req.mainData = {
       success: true,
-      newPassword: user.password,
-      message: "Password updated successfully"
-    });
+      data: task,
+      method: "updateTask",
+    };
+    next();
   } catch (error) {
     res.status(404).json({
       success: false,
       Error: error.message,
-      message: "Failed to update password"
     });
   }
 };
 
-exports.logout = async (req, res) => {
+exports.deleteTask = async (req, res, next) => {
   try {
-    const finderUser = await Users.findOne({
-      where: { token: req.user.token },
-    });
-    if (!finderUser || finderUser == "null") {
-      return res.status(400).json({
-        success: false,
-        Error: `please login again!`,
-        message: "Logout failed"
+    const { id } = req.params;
+
+    const task = await Tasks.findByPk(id);
+
+    if (!task) {
+      return res.status(404).json({
+        status: false,
+        Error: "Task not found!",
       });
     }
+    await task.destroy();
 
-    finderUser.token = null;
-    await finderUser.save();
-
-    res.status(201).json({
+    req.mainData = {
       success: true,
-      message: `logout User ${finderUser.name} successfully!`,
-      message: "Logout successful"
-    });
+      data: task,
+      method: "deleteTask",
+    };
+    next();
   } catch (error) {
     res.status(404).json({
       success: false,
       Error: error.message,
-      message: "Logout failed"
     });
   }
-};
-
-//CREATE TOKEN & SEND RESPONSE
-const sendTokenResponse = async (user, statusCode, res) => {
-  const token = user.getSignedJwtToken(user.id);
-  res.status(statusCode).json({
-    success: true,
-    token,
-    user,
-    message: "Login successful"
-  });
-  await Users.update({ token }, { where: { id: user.id } });
 };
