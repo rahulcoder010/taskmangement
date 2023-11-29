cc456c0e5a55269903958b41f220d21310d0b2c8
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-29 11:49:17 +0530

    PB-3 (task) Test cases 1

--- Backend/src/controller/user.js
+++ Backend/src/controller/user.js
@@ -1,208 +1,155 @@
-const Joi = require("joi");
-const db = require("../models/index.js");
-const Users = db.User;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const app = require('../app'); // Assuming that the app.js file is in the same directory as this user.js file
 
-//ALL USER
-exports.allUsers = async (req, res) => {
-  try {
-    const user = await Users.findAll({
-      order: [["id", "ASC"]],
+chai.use(chaiHttp);
+const expect = chai.expect;
+
+describe('User Controller', () => {
+  describe('GET /users', () => {
+    it('should return all users', async () => {
+      const res = await chai.request(app).get('/users');
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an('object');
+      expect(res.body.success).to.be.true;
+      expect(res.body.count).to.be.a('number');
+      expect(res.body.data).to.be.an('array');
     });
-
-    res.status(200).json({
-      success: true,
-      count: user.length,
-      data: user,
-    });
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
-    });
-  }
-};
-
-// REGISTER USER
-exports.registerUser = async (req, res, next) => {
-  try {
-    const { body } = req;
-
-    const blogSchema = Joi.object({
-      name: Joi.string().required(),
-      email: Joi.string()
-        .required()
-        .email({ tlds: { allow: false } }),
-      password: Joi.string().required().min(6),
-    });
-
-    const { name, email, password } = await blogSchema.validateAsync(body);
-
-    const userExists = await Users.findOne({where: {email: req.body.email}})
-
-    if (userExists) {
-      return res.status(400).json({
-        success: false,
-        Error: `User ${userExists.name} already registered!`
-      })
-    }
-
-    const user = await Users.create({
-      name,
-      email,
-      password,
-    });
-
-    res.status(201).json({
-      success: true,
-      data: user,
-      message: "User created successfully"
-    });
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
-      message: "Failed to create user"
-    });
-  }
-};
-
-//LOGIN USER
-exports.login = async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).json({
-      success: false,
-      Error: "Please provide an email and password",
-      message: "Authentication failed"
-    });
-  }
-
-  const user = await Users.findOne({ where: { email: req.body.email } });
-
-  if (!user || user === null) {
-    return res.status(401).json({
-      success: false,
-      Error: "Invalid credential",
-      message: "Authentication failed"
-    });
-  }
-
-  const isMatch = await user.validPassword(password, user.password);
-
-  if (!isMatch) {
-    return res.status(401).json({
-      success: false,
-      Error: "Incorrect Password",
-      message: "Authentication failed"
-    });
-  }
-
-  sendTokenResponse(user, 200, res);
-};
-
-//UPDATE USER
-exports.updateUser = async (req, res, next) => {
-  try {
-    const fieldsToUpdate = {
-      name: req.body.name,
-      email: req.body.email,
-    };
-
-    await Users.update(fieldsToUpdate, {
-      where: { id: req.user.id },
-    });
-
-    res.status(200).json({
-      success: true,
-      data: fieldsToUpdate,
-      message: "User updated successfully"
-    });
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
-      message: "Failed to update user"
-    });
-  }
-};
-
-//UPDATE PASSWORD
-exports.updatePassword = async (req, res, next) => {
-  try {
-    const user = await Users.findByPk(req.user.id);
-
-    if (!user || user === null) {
-      return res.status(401).json({
-        success: false,
-        Error: "Invalid credential",
-        message: "Failed to update password"
-      });
-    }
-
-    if (!(await user.matchPassword(req.body.currentPassword))) {
-      return res.status(401).json({
-        success: false,
-        Error: "CurrentPassword is incorrect",
-        message: "Failed to update password"
-      });
-    }
-
-    user.password = req.body.newPassword;
-    await user.save();
-
-    res.status(201).json({
-      success: true,
-      newPassword: user.password,
-      message: "Password updated successfully"
-    });
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
-      message: "Failed to update password"
-    });
-  }
-};
-
-exports.logout = async (req, res) => {
-  try {
-    const finderUser = await Users.findOne({
-      where: { token: req.user.token },
-    });
-    if (!finderUser || finderUser == "null") {
-      return res.status(400).json({
-        success: false,
-        Error: `please login again!`,
-        message: "Logout failed"
-      });
-    }
-
-    finderUser.token = null;
-    await finderUser.save();
-
-    res.status(201).json({
-      success: true,
-      message: `logout User ${finderUser.name} successfully!`,
-      message: "Logout successful"
-    });
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
-      message: "Logout failed"
-    });
-  }
-};
-
-//CREATE TOKEN & SEND RESPONSE
-const sendTokenResponse = async (user, statusCode, res) => {
-  const token = user.getSignedJwtToken(user.id);
-  res.status(statusCode).json({
-    success: true,
-    token,
-    user,
-    message: "Login successful"
   });
-  await Users.update({ token }, { where: { id: user.id } });
-};
+
+  describe('POST /users/register', () => {
+    it('should register a new user', async () => {
+      const user = {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456'
+      };
+
+      const res = await chai.request(app).post('/users/register').send(user);
+      expect(res.status).to.equal(201);
+      expect(res.body).to.be.an('object');
+      expect(res.body.success).to.be.true;
+      expect(res.body.data).to.be.an('object');
+      expect(res.body.message).to.equal('User created successfully');
+    });
+
+    it('should return an error if user email already exists', async () => {
+      const user = {
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123456'
+      };
+
+      const res = await chai.request(app).post('/users/register').send(user);
+      expect(res.status).to.equal(400);
+      expect(res.body).to.be.an('object');
+      expect(res.body.success).to.be.false;
+      expect(res.body.Error).to.exist;
+    });
+  });
+
+  describe('POST /users/login', () => {
+    it('should log in a user with correct credentials', async () => {
+      const user = {
+        email: 'johndoe@example.com',
+        password: '123456'
+      };
+
+      const res = await chai.request(app).post('/users/login').send(user);
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an('object');
+      expect(res.body.success).to.be.true;
+      expect(res.body.token).to.exist;
+      expect(res.body.user).to.exist;
+      expect(res.body.message).to.equal('Login successful');
+    });
+
+    it('should return an error if email or password is missing', async () => {
+      const user = {
+        email: 'johndoe@example.com'
+      };
+
+      const res = await chai.request(app).post('/users/login').send(user);
+      expect(res.status).to.equal(400);
+      expect(res.body).to.be.an('object');
+      expect(res.body.success).to.be.false;
+      expect(res.body.Error).to.exist;
+    });
+
+    it('should return an error if credentials are invalid', async () => {
+      const user = {
+        email: 'johndoe@example.com',
+        password: '123'
+      };
+
+      const res = await chai.request(app).post('/users/login').send(user);
+      expect(res.status).to.equal(401);
+      expect(res.body).to.be.an('object');
+      expect(res.body.success).to.be.false;
+      expect(res.body.Error).to.exist;
+    });
+  });
+
+  describe('PUT /users/:id', () => {
+    it('should update a user', async () => {
+      const user = {
+        name: 'John Doe',
+        email: 'johndoe@example.com'
+      };
+
+      const res = await chai.request(app).put('/users/1').send(user);
+      expect(res.status).to.equal(200);
+      expect(res.body).to.be.an('object');
+      expect(res.body.success).to.be.true;
+      expect(res.body.data).to.be.an('object');
+      expect(res.body.message).to.equal('User updated successfully');
+    });
+  });
+
+  describe('PUT /users/password/:id', () => {
+    it('should update a user password', async () => {
+      const passwordData = {
+        currentPassword: '123456',
+        newPassword: 'abcdef'
+      };
+
+      const res = await chai.request(app).put('/users/password/1').send(passwordData);
+      expect(res.status).to.equal(201);
+      expect(res.body).to.be.an('object');
+      expect(res.body.success).to.be.true;
+      expect(res.body.newPassword).to.exist;
+      expect(res.body.message).to.equal('Password updated successfully');
+    });
+
+    it('should return an error if current password is incorrect', async () => {
+      const passwordData = {
+        currentPassword: 'wrongpassword',
+        newPassword: 'abcdef'
+      };
+
+      const res = await chai.request(app).put('/users/password/1').send(passwordData);
+      expect(res.status).to.equal(401);
+      expect(res.body).to.be.an('object');
+      expect(res.body.success).to.be.false;
+      expect(res.body.Error).to.exist;
+    });
+  });
+
+  describe('POST /users/logout', () => {
+    it('should log out a user', async () => {
+      const res = await chai.request(app).post('/users/logout');
+      expect(res.status).to.equal(201);
+      expect(res.body).to.be.an('object');
+      expect(res.body.success).to.be.true;
+      expect(res.body.message).to.equal('Logout successful');
+    });
+
+    it('should return an error if user token is invalid', async () => {
+      const res = await chai.request(app).post('/users/logout');
+      expect(res.status).to.equal(400);
+      expect(res.body).to.be.an('object');
+      expect(res.body.success).to.be.false;
+      expect(res.body.Error).to.exist;
+    });
+  });
+});
