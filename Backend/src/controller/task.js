0b565bd3381999f74ff218af23313c128ab3f491
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-29 11:52:05 +0530

    PB-4 (task) Unit test

--- Backend/src/controller/task.js
+++ Backend/src/controller/task.js
@@ -117,3 +117,82 @@ exports.deleteTask = async (req, res, next) => {
     });
   }
 };
+
+// Unit test cases for above file Backend/src/controller/task.js
+
+const request = require('supertest')
+const app = require('../app')
+
+describe('Task Controller', () => {
+  it('Get all tasks', async () => {
+    const res = await request(app).get('/tasks')
+    expect(res.statusCode).toEqual(200)
+    expect(res.body.success).toBe(true)
+    expect(res.body.count).toEqual(expect.any(Number))
+    expect(res.body.data).toEqual(expect.any(Array))
+  })
+
+  it('Add a task', async () => {
+    const res = await request(app)
+      .post('/tasks')
+      .send({
+        title: 'Task 1',
+        description: 'This is task 1'
+      })
+    expect(res.statusCode).toEqual(200)
+    expect(res.body.success).toBe(true)
+    expect(res.body.data.title).toEqual('Task 1')
+    expect(res.body.data.description).toEqual('This is task 1')
+  })
+
+  it('Update a task', async () => {
+    const id = 1
+    const res = await request(app)
+      .put(`/tasks/${id}`)
+      .send({
+        status: 'completed'
+      })
+    expect(res.statusCode).toEqual(200)
+    expect(res.body.success).toBe(true)
+    expect(res.body.data.id).toEqual(id)
+    expect(res.body.data.status).toEqual('completed')
+  })
+
+  it('Update a task with invalid ID', async () => {
+    const id = 100
+    const res = await request(app)
+      .put(`/tasks/${id}`)
+      .send({
+        status: 'completed'
+      })
+    expect(res.statusCode).toEqual(404)
+    expect(res.body.success).toBe(false)
+    expect(res.body.message).toEqual('Task not found!')
+  })
+
+  it('Update a task without status', async () => {
+    const id = 1
+    const res = await request(app)
+      .put(`/tasks/${id}`)
+      .send({})
+    expect(res.statusCode).toEqual(404)
+    expect(res.body.success).toBe(false)
+    expect(res.body.message).toEqual('Please add status in body!')
+  })
+
+  it('Delete a task', async () => {
+    const id = 1
+    const res = await request(app).delete(`/tasks/${id}`)
+    expect(res.statusCode).toEqual(200)
+    expect(res.body.success).toBe(true)
+    expect(res.body.data.id).toEqual(id)
+  })
+
+  it('Delete a task with invalid ID', async () => {
+    const id = 100
+    const res = await request(app).delete(`/tasks/${id}`)
+    expect(res.statusCode).toEqual(404)
+    expect(res.body.success).toBe(false)
+    expect(res.body.Error).toEqual('Task not found!')
+  })
+})
