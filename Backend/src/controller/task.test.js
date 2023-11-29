67c0e00c9168ad74462e2561e5bf9d89cd18a876
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-11-29 11:57:25 +0530

    PB-1 (task) Test Error

---
+++ Backend/src/controller/task.test.js
@@ -0,0 +1,21 @@
+const TaskController = require("../controllers/task");
+
+describe("TaskController", () => {
+  describe("createTask", () => {
+    it("should create a new task", () => {
+      // add test logic here
+    });
+  });
+
+  describe("updateTask", () => {
+    it("should update an existing task", () => {
+      // add test logic here
+    });
+  });
+
+  describe("deleteTask", () => {
+    it("should delete a task", () => {
+      // add test logic here
+    });
+  });
+});
