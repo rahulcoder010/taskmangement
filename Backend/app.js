92f9e36a7eb98cefc08a6e2a000f0f3cae03c9c1
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-11 10:53:15 +0530

    PB-11 (task) test cond2

--- Backend/app.js
+++ Backend/app.js
@@ -1,42 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+
 const app = express();
-
-app.use(express.json());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
-
-require("./src/routes/index.js")(app);
+const http = require("http");
+const socketIO = require("socket.io");
 
 const PORT = 5000;
-
-const http = require("http");
 const server = http.createServer(app);
-const socketIO = require("socket.io");
 const io = socketIO(server, {
   cors: {
     origin: "http://localhost:3000",
   },
 });
 
-io.on("connection", (so) => {
+app.use(express.json());
+app.use(cors({
+  origin: "*",
+}));
+
+require("./src/routes/index.js")(app);
+
+io.on("connection", (socket) => {
   console.log("New client connected");
 
-  so.on("disconnect", (interval) => {
+  socket.on("disconnect", (interval) => {
     console.log("Client disconnected");
     clearInterval(interval);
   });
 });
 
 app.use((req, res) => {
-  if(req?.mainData?.method==="addTask"){
+  if (req?.mainData?.method === "addTask") {
     io.sockets.emit("addTask", req.mainData.data);
-  }else if(req?.mainData?.method==="updateTask"){
+  } else if (req?.mainData?.method === "updateTask") {
     io.sockets.emit("updateTask", req.mainData.data);
-  }else if(req?.mainData?.method==="deleteTask"){
+  } else if (req?.mainData?.method === "deleteTask") {
     io.sockets.emit("deleteTask", req.mainData.data);
   }
   res.json({
