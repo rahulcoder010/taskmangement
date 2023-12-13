01b141093d995ba9ba2b168f292fa3f2518189b8
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-13 21:06:55 +0530

    PB-1 (task) Test Cases

--- Backend/src/controller/task.js
+++ Backend/src/controller/task.js
@@ -1,119 +1,142 @@
-const db = require("../models/index.js");
-const Tasks = db.Task;
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const expect = chai.expect;
+chai.use(chaiHttp);
 
-exports.allTasks = async (req, res) => {
-  try {
-    if (!req.user.token) {
-      return res.status(400).json({
-        success: false,
-        Error: "**Please login again!**",
-      });
-    }
-    const tasks = await Tasks.findAll({
-      order: [["id", "ASC"]],
+describe('Task API', () => {
+  describe('GET /tasks', () => {
+    it('should get all tasks', (done) => {
+      chai.request('http://localhost:3000')
+        .get('/tasks')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.success).to.equal(true);
+          expect(res.body.data).to.be.an('array');
+          done();
+        });
+    });
+  });
+
+  describe('POST /tasks', () => {
+    it('should add a new task', (done) => {
+      chai.request('http://localhost:3000')
+        .post('/tasks')
+        .send({
+          title: 'Test Task',
+          description: 'This is a test task'
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.success).to.equal(true);
+          expect(res.body.data).to.be.an('object');
+          done();
+        });
     });
 
-    res.status(200).json({
-      success: true,
-      count: tasks.length,
-      data: tasks,
+    it('should return an error if title or description is missing', (done) => {
+      chai.request('http://localhost:3000')
+        .post('/tasks')
+        .send({
+          title: '',
+          description: ''
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body).to.be.an('object');
+          expect(res.body.success).to.equal(false);
+          expect(res.body.Error).to.equal('**title & description are required!**');
+          done();
+        });
     });
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
+
+    it('should return an error if title or description length is too long', (done) => {
+      chai.request('http://localhost:3000')
+        .post('/tasks')
+        .send({
+          title: 'This is a very long title that exceeds the character limit',
+          description: 'This is a very long description that exceeds the character limit'
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body).to.be.an('object');
+          expect(res.body.success).to.equal(false);
+          expect(res.body.Error).to.equal('**Title or description length too long!**');
+          done();
+        });
     });
-  }
-};
+  });
 
-exports.addTask = async (req, res, next) => {
-  try {
-    const { title, description } = req.body;
-    if (!title || !description) {
-      return res.status(400).json({
-        success: false,
-        Error: "**title & description are required!**",
-      });
-    }
-    if (title.length >= 50 || description.length >= 200) {
-      return res.status(400).json({
-        success: false,
-        Error: "**Title or description length too long!**",
-      });
-    }
-
-    const task = await Tasks.create({ title, description });
-
-    req.mainData = {
-      success: true,
-      data: task,
-      method: "addTask",
-    };
-    next();
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
+  describe('PUT /tasks/:id', () => {
+    it('should update the status of a task', (done) => {
+      chai.request('http://localhost:3000')
+        .put('/tasks/1')
+        .send({
+          status: 'completed'
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.success).to.equal(true);
+          expect(res.body.data).to.be.an('object');
+          expect(res.body.data.status).to.equal('completed');
+          done();
+        });
     });
-  }
-};
 
-exports.updateTask = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { status } = req.body;
-    const task = await Tasks.findByPk(id);
-    if (!task) {
-      return res
-        .status(404)
-        .json({ status: false, message: "Task not found!" });
-    }
-    if (!status) {
-      return res
-        .status(404)
-        .json({ status: false, message: "Please add status in body!" });
-    }
-    task.status = status;
-    await task.save();
-
-    req.mainData = {
-      success: true,
-      data: task,
-      method: "updateTask",
-    };
-    next();
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
+    it('should return an error if task is not found', (done) => {
+      chai.request('http://localhost:3000')
+        .put('/tasks/99')
+        .send({
+          status: 'completed'
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          expect(res.body).to.be.an('object');
+          expect(res.body.status).to.equal(false);
+          expect(res.body.message).to.equal('Task not found!');
+          done();
+        });
     });
-  }
-};
 
-exports.deleteTask = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-
-    const task = await Tasks.findByPk(id);
-
-    if (!task) {
-      return res.status(404).json({
-        status: false,
-        Error: "Task not found!",
-      });
-    }
-    await task.destroy();
-
-    req.mainData = {
-      success: true,
-      data: task,
-      method: "deleteTask",
-    };
-    next();
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
+    it('should return an error if status is missing in the request body', (done) => {
+      chai.request('http://localhost:3000')
+        .put('/tasks/1')
+        .send({})
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          expect(res.body).to.be.an('object');
+          expect(res.body.status).to.equal(false);
+          expect(res.body.message).to.equal('Please add status in body!');
+          done();
+        });
     });
-  }
-};
+  });
+
+  describe('DELETE /tasks/:id', () => {
+    it('should delete a task', (done) => {
+      chai.request('http://localhost:3000')
+        .delete('/tasks/1')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.success).to.equal(true);
+          expect(res.body.data).to.be.an('object');
+          done();
+        });
+    });
+
+    it('should return an error if task is not found', (done) => {
+      chai.request('http://localhost:3000')
+        .delete('/tasks/99')
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          expect(res.body).to.be.an('object');
+          expect(res.body.status).to.equal(false);
+          expect(res.body.Error).to.equal('Task not found!');
+          done();
+        });
+    });
+  });
+});
