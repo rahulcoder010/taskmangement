30480101e2727a1ddd525b90d721cfb733c56b50
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-18 12:41:49 +0530

    PB-7 (task)  test 3

---
+++ Frontend/src/Register.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Implement register logic here
+  
+    // After successful registration, redirect to login page
+    history.push('/login');
+  };
+
+  return (
+    <div>
+      <h2>Register</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Username:
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        </label>
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+        <label>
+          Password:
+          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
