0a1a2401174214677933bd200c86191036337365
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-18 12:41:51 +0530

    PB-2 (task) Test Cases

--- Backend/src/controller/task.js
+++ Backend/src/controller/task.js
@@ -1,119 +1,179 @@
-const db = require("../models/index.js");
-const Tasks = db.Task;
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const app = require("../app");
+chai.use(chaiHttp);
+chai.should();
 
-exports.allTasks = async (req, res) => {
-  try {
-    if (!req.user.token) {
-      return res.status(400).json({
-        success: false,
-        Error: "**Please login again!**",
-      });
-    }
-    const tasks = await Tasks.findAll({
-      order: [["id", "ASC"]],
+describe("Task API", () => {
+  before((done) => {
+    // This code will run before every test
+    done();
+  });
+
+  after((done) => {
+    // This code will run after every test
+    done();
+  });
+
+  describe("GET /tasks", () => {
+    it("should get all tasks", (done) => {
+      chai
+        .request(app)
+        .get("/tasks")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property("success").eq(true);
+          res.body.should.have.property("count");
+          res.body.should.have.property("data");
+          done();
+        });
+    });
+  });
+
+  describe("POST /task", () => {
+    it("should add a task", (done) => {
+      const task = {
+        title: "Test Task",
+        description: "This is a test task",
+      };
+      chai
+        .request(app)
+        .post("/task")
+        .send(task)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property("success").eq(true);
+          res.body.should.have.property("data");
+          res.body.data.should.have.property("title").eq("Test Task");
+          res.body.data.should.have.property("description").eq(
+            "This is a test task"
+          );
+          done();
+        });
     });
 
-    res.status(200).json({
-      success: true,
-      count: tasks.length,
-      data: tasks,
+    it("should return an error if title or description is missing", (done) => {
+      const task = {
+        title: "",
+        description: "This is a test task",
+      };
+      chai
+        .request(app)
+        .post("/task")
+        .send(task)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a("object");
+          res.body.should.have.property("success").eq(false);
+          res.body.should.have
+            .property("Error")
+            .eq("**title & description are required!**");
+          done();
+        });
     });
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
+
+    it("should return an error if title or description is too long", (done) => {
+      const task = {
+        title: "Task with a very long title that exceeds the character limit",
+        description: "This is a test task",
+      };
+      chai
+        .request(app)
+        .post("/task")
+        .send(task)
+        .end((err, res) => {
+          res.should.have.status(400);
+          res.body.should.be.a("object");
+          res.body.should.have.property("success").eq(false);
+          res.body.should.have
+            .property("Error")
+            .eq("**Title or description length too long!**");
+          done();
+        });
     });
-  }
-};
+  });
 
-exports.addTask = async (req, res, next) => {
-  try {
-    const { title, description } = req.body;
-    if (!title || !description) {
-      return res.status(400).json({
-        success: false,
-        Error: "**title & description are required!**",
-      });
-    }
-    if (title.length >= 50 || description.length >= 200) {
-      return res.status(400).json({
-        success: false,
-        Error: "**Title or description length too long!**",
-      });
-    }
-
-    const task = await Tasks.create({ title, description });
-
-    req.mainData = {
-      success: true,
-      data: task,
-      method: "addTask",
-    };
-    next();
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
+  describe("PUT /task/:id", () => {
+    it("should update a task", (done) => {
+      const task = {
+        status: "completed",
+      };
+      chai
+        .request(app)
+        .put("/task/1")
+        .send(task)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property("success").eq(true);
+          res.body.should.have.property("data");
+          res.body.data.should.have.property("status").eq("completed");
+          done();
+        });
     });
-  }
-};
 
-exports.updateTask = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { status } = req.body;
-    const task = await Tasks.findByPk(id);
-    if (!task) {
-      return res
-        .status(404)
-        .json({ status: false, message: "Task not found!" });
-    }
-    if (!status) {
-      return res
-        .status(404)
-        .json({ status: false, message: "Please add status in body!" });
-    }
-    task.status = status;
-    await task.save();
-
-    req.mainData = {
-      success: true,
-      data: task,
-      method: "updateTask",
-    };
-    next();
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
+    it("should return an error if task with the given id is not found", (done) => {
+      const task = {
+        status: "completed",
+      };
+      chai
+        .request(app)
+        .put("/task/100")
+        .send(task)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          res.body.should.have.property("success").eq(false);
+          res.body.should.have.property("Error").eq("Task not found!");
+          done();
+        });
     });
-  }
-};
 
-exports.deleteTask = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-
-    const task = await Tasks.findByPk(id);
-
-    if (!task) {
-      return res.status(404).json({
-        status: false,
-        Error: "Task not found!",
-      });
-    }
-    await task.destroy();
-
-    req.mainData = {
-      success: true,
-      data: task,
-      method: "deleteTask",
-    };
-    next();
-  } catch (error) {
-    res.status(404).json({
-      success: false,
-      Error: error.message,
+    it("should return an error if status is not provided in the body", (done) => {
+      const task = {};
+      chai
+        .request(app)
+        .put("/task/1")
+        .send(task)
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          res.body.should.have.property("success").eq(false);
+          res.body.should.have
+            .property("Error")
+            .eq("Please add status in body!");
+          done();
+        });
     });
-  }
-};
+  });
+
+  describe("DELETE /task/:id", () => {
+    it("should delete a task", (done) => {
+      chai
+        .request(app)
+        .delete("/task/1")
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a("object");
+          res.body.should.have.property("success").eq(true);
+          res.body.should.have.property("data");
+          done();
+        });
+    });
+
+    it("should return an error if task with the given id is not found", (done) => {
+      chai
+        .request(app)
+        .delete("/task/100")
+        .end((err, res) => {
+          res.should.have.status(404);
+          res.body.should.be.a("object");
+          res.body.should.have.property("success").eq(false);
+          res.body.should.have.property("Error").eq("Task not found!");
+          done();
+        });
+    });
+  });
+});
