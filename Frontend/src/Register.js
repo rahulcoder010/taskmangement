ff5b1bc80bf1941f86c479c5874907cd3ea696ad
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-18 12:42:51 +0530

    PB-7 (task)  test 3

---
+++ Frontend/src/Register.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    // TODO: Add code to handle form submission
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <br />
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <br />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register;
