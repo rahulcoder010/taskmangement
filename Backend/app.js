0f50f6f7777e1207c5203c3fcadd148ff11ff92f
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2023-12-25 15:56:25 +0530

    PB-22 (task) Test Error

--- Backend/app.js
+++ Backend/app.js
@@ -9,10 +9,6 @@ app.use(
   })
 );
 
-require("./src/routes/index.js")(app);
-
-const PORT = 5000;
-
 const http = require("http");
 const server = http.createServer(app);
 const socketIO = require("socket.io");
@@ -22,21 +18,21 @@ const io = socketIO(server, {
   },
 });
 
-io.on("connection", (so) => {
+io.on("connection", (socket) => {
   console.log("New client connected");
 
-  so.on("disconnect", (interval) => {
+  socket.on("disconnect", (interval) => {
     console.log("Client disconnected");
     clearInterval(interval);
   });
 });
 
 app.use((req, res) => {
-  if(req?.mainData?.method==="addTask"){
+  if (req?.mainData?.method === "addTask") {
     io.sockets.emit("addTask", req.mainData.data);
-  }else if(req?.mainData?.method==="updateTask"){
+  } else if (req?.mainData?.method === "updateTask") {
     io.sockets.emit("updateTask", req.mainData.data);
-  }else if(req?.mainData?.method==="deleteTask"){
+  } else if (req?.mainData?.method === "deleteTask") {
     io.sockets.emit("deleteTask", req.mainData.data);
   }
   res.json({
@@ -48,6 +44,7 @@ app.use("*", (req, res) => {
   res.send("not found");
 });
 
+const PORT = 5000;
 server.listen(PORT, () => {
   console.log(`server running on port ${PORT}`);
 });
